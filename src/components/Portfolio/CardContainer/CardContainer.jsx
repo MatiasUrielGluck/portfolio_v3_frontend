dc2379d1520f0d77d58d8bfae946fb34562625d8
MatiasUrielGluck <matiasugluck@gmail.com>
2022-10-12 22:24:33 -0300

    Note

--- src/components/Portfolio/CardContainer/CardContainer.jsx
+++ src/components/Portfolio/CardContainer/CardContainer.jsx
@@ -3,7 +3,7 @@ import './cardcontainer.css'
 export const CardContainer = () => {
   return (
     <div className='card-container container'>
-      
+      {/* Calculate card amount by window size */}
     </div>
   )
 }
