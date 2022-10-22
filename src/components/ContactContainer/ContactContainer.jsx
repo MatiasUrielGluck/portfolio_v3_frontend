050ef98084d26740865d9be304a5e494a6e9fa7c
MatiasUrielGluck <matiasugluck@gmail.com>
2022-10-22 08:17:57 -0300

    Details

--- src/components/ContactContainer/ContactContainer.jsx
+++ src/components/ContactContainer/ContactContainer.jsx
@@ -8,11 +8,20 @@ export const ContactContainer = () => {
         <a href="mailto:matiasugluck@gmail.com" target="_blank">
           <i className="fa-sharp fa-solid fa-envelope fa-3x"></i>
         </a>
-        <a href="https://www.linkedin.com/in/mat%C3%ADas-uriel-gluck?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B1W0DyPbpQayzqK6h9OmqMQ%3D%3D" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/mat%C3%ADas-uriel-gluck?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B1W0DyPbpQayzqK6h9OmqMQ%3D%3D"
+          target="_blank"
+        >
           <i className="fa-brands fa-linkedin fa-3x"></i>
         </a>
         <a href="https://github.com/MatiasUrielGluck" target="_blank">
           <i className="fa-brands fa-square-github fa-3x"></i>
+        </a>
+        <a
+          href="https://www.youtube.com/channel/UCKbXB3qWadl9qUKiBF6i8Gw"
+          target="_blank"
+        >
+          <i class="fa-brands fa-square-youtube fa-3x"></i>
         </a>
         <a href="https://www.instagram.com/matias_uriel_gluck/" target="_blank">
           <i className="fa-brands fa-square-instagram fa-3x"></i>
